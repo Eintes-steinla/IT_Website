0e3747f09b04f99bc39c107315bc2861c7e38557
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-03-06 09:30:50 +0700

    hide show password

--- pages/sign-up.php
+++ pages/sign-up.php
@@ -90,12 +90,56 @@
                         id="email"
                         class="bg-input mb-4 p-2 border-[1px] border-input rounded-lg focus:outline-none h-8">
 
+                    <!-- * input password -->
                     <label for="password" class="pl-2 md:pl-0 font-[exo2-bold]">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        class="bg-input mb-4 p-2 border-[1px] border-input rounded-lg focus:outline-none h-8">
+                    <div class="relative w-full">
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            class="bg-input mb-4 py-2 pr-9 pl-2 border-[1px] border-input rounded-lg focus:outline-none w-full h-8">
+
+                        <svg
+                            id="show-password"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="top-2 right-3 absolute cursor-pointer"
+                            viewBox="0 0 16 16">
+                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
+                            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
+                        </svg>
+
+                        <svg
+                            id="hide-password"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="hidden top-2 right-3 absolute cursor-pointer"
+                            viewBox="0 0 16 16">
+                            <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z" />
+                            <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z" />
+                        </svg>
+                    </div>
+                    <script>
+                        const showPassword = document.getElementById('show-password');
+                        const hidePassword = document.getElementById('hide-password');
+                        const password = document.getElementById('password');
+
+                        showPassword.addEventListener('click', () => {
+                            password.type = 'text';
+                            showPassword.classList.add('hidden');
+                            hidePassword.classList.remove('hidden');
+                        });
+
+                        hidePassword.addEventListener('click', () => {
+                            password.type = 'password';
+                            showPassword.classList.remove('hidden');
+                            hidePassword.classList.add('hidden');
+                        });
+                    </script>
 
                     <label for="date" class="pl-2 md:pl-0 font-[exo2-bold]">Date of Birth</label>
                     <input
