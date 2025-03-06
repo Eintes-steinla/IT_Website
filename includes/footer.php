325c1e752380afee431e9b5ff443672674357b2a
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-03-06 19:17:16 +0700

    link social

--- includes/footer.php
+++ includes/footer.php
@@ -5,8 +5,8 @@
     <div class="bg-stars stars--large"></div>
 
     <div class="gap-4 md:gap-2 xl:gap-0 grid grid-cols-1 md:grid-cols-3 grid-rows-3 md:grid-rows-1 px-4 py-15 md:py-25 w-full h-full">
-        <div class="flex flex-col justify-center items-center">
-            <div class="group relative flex flex-col justify-between items-start gap-2 bg-nav hover:shadow-xl p-4 rounded-lg w-full max-w-[370px] h-56 hover:-translate-y-2 duration-500">
+        <div class="flex justify-center items-center">
+            <div class="group relative flex flex-col justify-between items-start gap-2 bg-card shadow-[6px_6px_10px_rgba(0,0,0,0.15)] hover:shadow-xl p-4 rounded-lg w-full max-w-[370px] h-56 transition-all hover:-translate-y-2 duration-500 ease-in-out">
                 <div
                     class="right-0 bottom-0 absolute bg-transparent shadow-md rounded-lg w-1/3 group-hover:-translate-y-2 group-hover:-translate-x-2 duration-700">
                     <img src="../assets/images/img/deku.png" alt="deku">
@@ -15,26 +15,28 @@
                 <div class="">
                     <h2 class="mb-2 font-bold text-normal text-lg">Bao Minh</h2>
                     <p class="text-note text-sm line-clamp-3">
-                        hi i am a student and i am learning web development and i am a beginner Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium libero eos debitis soluta rerum at explicabo repellat magni vitae! Odio, cumque sequi? Perferendis consequatur porro provident itaque beatae! Facilis, ducimus.
-                        Iusto assumenda explicabo eaque reprehenderit. Facilis nostrum molestias cupiditate illum magnam aspernatur nemo, animi quos nesciunt similique rerum quaerat fugiat amet corrupti consectetur velit alias error possimus accusantium! Voluptatem, dolorum.
+                        Hi there! i'm a web developer and designer. i love to create beautiful and functional websites for my clients.
                     </p>
                 </div>
                 <!-- From Uiverse.io by adamgiebl -->
-                <button style="background: linear-gradient(to right, #8e2de2, #4a00e0);" class="relative border-none rounded-2xl overflow-hidden font-[exo2-bold] text-md text-normal active:scale-95 transition-all duration-300 ease-in-out cursor-pointer cssbuttons-io">
-                    <span class="inline-flex z-10 relative items-center px-4 py-3 transition-colors duration-500">
-                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-current mr-2 w-5 h-5">
-                            <path d="M0 0h24v24H0z" fill="none"></path>
-                            <path
-                                d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"
-                                fill="currentColor"></path>
+                <button style="background: linear-gradient(to right, #8e2de2, #4a00e0);" class="relative shadow-[0px_2px_15px_rgba(100,100,100,0.35)] border-none rounded-4xl overflow-hidden font-[exo2-bold] text-md text-normal active:scale-95 transition-all duration-300 ease-in-out cursor-pointer cssbuttons-io">
+                    <a href="https://github.com/Eintes-steinla" class="inline-flex z-10 relative items-center p-3.5 transition-colors duration-500">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="25"
+                            height="25"
+                            fill=""
+                            class="fill-white"
+                            viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
                         </svg>
-                        Code</span>
+                    </a>
                 </button>
             </div>
         </div>
 
-        <div class="flex flex-col justify-center items-center">
-            <div class="flex justify-center items-center bg-nav rounded-lg w-full max-w-[370px] h-full max-h-56 transition-all duration-300 ease-in-out">
+        <div class="flex justify-center items-center">
+            <div class="flex justify-center items-center bg-card shadow-[6px_6px_10px_rgba(0,0,0,0.15)] rounded-lg w-full max-w-[370px] h-full max-h-56 transition-all hover:-translate-y-2 duration-500 ease-in-out">
                 <!-- From Uiverse.io by Ashon-G -->
                 <!-- <svg
                     viewBox="0 0 128 128"
@@ -122,55 +124,43 @@
 
                 <div class="flex flex-col gap-3">
                     <div class="flex flex-row gap-3">
-                        <button
-                            class="group flex justify-center items-center bg-[#201236] hover:bg-[#cc39a4] shadow-lg border-[1px] border-input rounded-[0px_0px_0px_0px] outline-none w-[60px] h-[60px] hover:scale-105 active:scale-95 skew-5 transition-all hover:-translate-x-1 hover:-translate-y-1 duration-400 ease-in-out cursor-pointer">
-                            <svg
-                                class="fill-white -skew-5"
-                                height="30"
-                                width="30"
-                                viewBox="0,0,256,256"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <g stroke="none">
-                                    <g transform="scale(8,8)">
-                                        <path
-                                            d="M11.46875,5c-3.55078,0 -6.46875,2.91406 -6.46875,6.46875v9.0625c0,3.55078 2.91406,6.46875 6.46875,6.46875h9.0625c3.55078,0 6.46875,-2.91406 6.46875,-6.46875v-9.0625c0,-3.55078 -2.91406,-6.46875 -6.46875,-6.46875zM11.46875,7h9.0625c2.47266,0 4.46875,1.99609 4.46875,4.46875v9.0625c0,2.47266 -1.99609,4.46875 -4.46875,4.46875h-9.0625c-2.47266,0 -4.46875,-1.99609 -4.46875,-4.46875v-9.0625c0,-2.47266 1.99609,-4.46875 4.46875,-4.46875zM21.90625,9.1875c-0.50391,0 -0.90625,0.40234 -0.90625,0.90625c0,0.50391 0.40234,0.90625 0.90625,0.90625c0.50391,0 0.90625,-0.40234 0.90625,-0.90625c0,-0.50391 -0.40234,-0.90625 -0.90625,-0.90625zM16,10c-3.30078,0 -6,2.69922 -6,6c0,3.30078 2.69922,6 6,6c3.30078,0 6,-2.69922 6,-6c0,-3.30078 -2.69922,-6 -6,-6zM16,12c2.22266,0 4,1.77734 4,4c0,2.22266 -1.77734,4 -4,4c-2.22266,0 -4,-1.77734 -4,-4c0,-2.22266 1.77734,-4 4,-4z"></path>
+                        <!-- ig -->
+                        <a href="https://www.instagram.com/bminhh._.26/">
+                            <button
+                                class="group flex justify-center items-center bg-[#201236] shadow-lg border-[1px] border-input rounded-[0px_0px_0px_0px] outline-none w-[60px] h-[60px] hover:scale-105 active:scale-95 skew-5 transition-all hover:-translate-x-1 hover:-translate-y-1 duration-400 ease-in-out cursor-pointer">
+                                <div class="absolute opacity-0 instagram-gradient w-full h-full transition-all duration-400 ease-in-out"></div>
+                                <svg
+                                    class="fill-white -skew-5"
+                                    height="30"
+                                    width="30"
+                                    viewBox="0,0,256,256"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <g stroke="none">
+                                        <g transform="scale(8,8)">
+                                            <path
+                                                d="M11.46875,5c-3.55078,0 -6.46875,2.91406 -6.46875,6.46875v9.0625c0,3.55078 2.91406,6.46875 6.46875,6.46875h9.0625c3.55078,0 6.46875,-2.91406 6.46875,-6.46875v-9.0625c0,-3.55078 -2.91406,-6.46875 -6.46875,-6.46875zM11.46875,7h9.0625c2.47266,0 4.46875,1.99609 4.46875,4.46875v9.0625c0,2.47266 -1.99609,4.46875 -4.46875,4.46875h-9.0625c-2.47266,0 -4.46875,-1.99609 -4.46875,-4.46875v-9.0625c0,-2.47266 1.99609,-4.46875 4.46875,-4.46875zM21.90625,9.1875c-0.50391,0 -0.90625,0.40234 -0.90625,0.90625c0,0.50391 0.40234,0.90625 0.90625,0.90625c0.50391,0 0.90625,-0.40234 0.90625,-0.90625c0,-0.50391 -0.40234,-0.90625 -0.90625,-0.90625zM16,10c-3.30078,0 -6,2.69922 -6,6c0,3.30078 2.69922,6 6,6c3.30078,0 6,-2.69922 6,-6c0,-3.30078 -2.69922,-6 -6,-6zM16,12c2.22266,0 4,1.77734 4,4c0,2.22266 -1.77734,4 -4,4c-2.22266,0 -4,-1.77734 -4,-4c0,-2.22266 1.77734,-4 4,-4z"></path>
+                                        </g>
                                     </g>
-                                </g>
-                            </svg>
-                        </button>
-
-                        <button
-                            class="group flex justify-center items-center bg-[#201236] hover:bg-[#03A9F4] shadow-lg border-[1px] border-input rounded-[0px_0px_0px_0px] outline-none w-[60px] h-[60px] hover:scale-105 active:scale-95 -skew-5 transition-all hover:translate-x-1 hover:-translate-y-1 duration-400 ease-in-out cursor-pointer">
-                            <svg
-                                class="fill-white skew-5"
-                                height="30"
-                                width="30"
-                                viewBox="0 0 48 48"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M42,12.429c-1.323,0.586-2.746,0.977-4.247,1.162c1.526-0.906,2.7-2.351,3.251-4.058c-1.428,0.837-3.01,1.452-4.693,1.776C34.967,9.884,33.05,9,30.926,9c-4.08,0-7.387,3.278-7.387,7.32c0,0.572,0.067,1.129,0.193,1.67c-6.138-0.308-11.582-3.226-15.224-7.654c-0.64,1.082-1,2.349-1,3.686c0,2.541,1.301,4.778,3.285,6.096c-1.211-0.037-2.351-0.374-3.349-0.914c0,0.022,0,0.055,0,0.086c0,3.551,2.547,6.508,5.923,7.181c-0.617,0.169-1.269,0.263-1.941,0.263c-0.477,0-0.942-0.054-1.392-0.135c0.94,2.902,3.667,5.023,6.898,5.086c-2.528,1.96-5.712,3.134-9.174,3.134c-0.598,0-1.183-0.034-1.761-0.104C9.268,36.786,13.152,38,17.321,38c13.585,0,21.017-11.156,21.017-20.834c0-0.317-0.01-0.633-0.025-0.945C39.763,15.197,41.013,13.905,42,12.429"></path>
-                            </svg>
-                        </button>
+                                </svg>
+                            </button>
+                        </a>
+                        <!-- fb -->
+                        <a href="https://www.facebook.com/entes.steinla?locale=vi_VN">
+                            <button
+                                class="group flex justify-center items-center bg-[#201236] hover:bg-[#1877F2] shadow-lg border-[1px] border-input rounded-[0px_0px_0px_0px] outline-none w-[60px] h-[60px] hover:scale-105 active:scale-95 -skew-5 transition-all hover:translate-x-1 hover:-translate-y-1 duration-400 ease-in-out cursor-pointer">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="" class="fill-white skew-5" viewBox="0 0 16 16">
+                                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+                                </svg>
+                            </button>
+                        </a>
                     </div>
 
                     <div class="flex flex-row gap-3">
+                        <!-- discord -->
                         <button
-                            class="group flex justify-center items-center bg-[#201236] hover:bg-black shadow-lg border-[1px] border-input rounded-[0px_0px_0px_0px] outline-none w-[60px] h-[60px] hover:scale-105 active:scale-95 -skew-5 transition-all hover:-translate-x-1 hover:translate-y-1 duration-400 ease-in-out cursor-pointer">
+                            class="group flex justify-center items-center bg-[#201236] hover:bg-[#738ADB] shadow-lg border-[1px] border-input rounded-[0px_0px_0px_0px] outline-none w-[60px] h-[60px] hover:scale-105 active:scale-95 -skew-5 transition-all hover:-translate-x-1 hover:translate-y-1 duration-400 ease-in-out cursor-pointer">
                             <svg
                                 class="fill-white skew-5"
-                                height="30"
-                                width="30"
-                                viewBox="0 0 30 30"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
-                            </svg>
-                        </button>
-
-                        <button
-                            class="group flex justify-center items-center bg-[#201236] hover:bg-[#8c9eff] shadow-lg border-[1px] border-input rounded-[0px_0px_0px_0px] outline-none w-[60px] h-[60px] hover:scale-105 active:scale-95 skew-5 transition-all hover:translate-x-1 hover:translate-y-1 duration-400 ease-in-out cursor-pointer">
-                            <svg
-                                class="fill-white -skew-5"
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 48 48"
                                 width="30"
@@ -179,14 +169,26 @@
                                     d="M40,12c0,0-4.585-3.588-10-4l-0.488,0.976C34.408,10.174,36.654,11.891,39,14c-4.045-2.065-8.039-4-15-4s-10.955,1.935-15,4c2.346-2.109,5.018-4.015,9.488-5.024L18,8c-5.681,0.537-10,4-10,4s-5.121,7.425-6,22c5.162,5.953,13,6,13,6l1.639-2.185C13.857,36.848,10.715,35.121,8,32c3.238,2.45,8.125,5,16,5s12.762-2.55,16-5c-2.715,3.121-5.857,4.848-8.639,5.815L33,40c0,0,7.838-0.047,13-6C45.121,19.425,40,12,40,12z M17.5,30c-1.933,0-3.5-1.791-3.5-4c0-2.209,1.567-4,3.5-4s3.5,1.791,3.5,4C21,28.209,19.433,30,17.5,30z M30.5,30c-1.933,0-3.5-1.791-3.5-4c0-2.209,1.567-4,3.5-4s3.5,1.791,3.5,4C34,28.209,32.433,30,30.5,30z"></path>
                             </svg>
                         </button>
+                        <!-- youtube -->
+                        <button
+                            class="group flex justify-center items-center hover:bg-[#FF0000] shadow-lg border-[1px] border-input rounded-[0px_0px_0px_0px] outline-none w-[60px] h-[60px] hover:scale-105 active:scale-95 skew-5 transition-all hover:translate-x-1 hover:translate-y-1 duration-400 ease-in-out cursor-pointer">
+                            <svg
+                                class="fill-white -skew-5"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="25"
+                                height="25"
+                                fill=""
+                                viewBox="0 0 16 16">
+                                <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z" />
+                            </svg>
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="flex justify-center items-center">
-            <div
-                class="group relative flex justify-center items-center bg-main w-full max-w-[370px] h-full max-h-56 aspect-video transition-all duration-700">
+            <div class="group relative flex justify-center items-center bg-main shadow-[6px_6px_10px_rgba(0,0,0,0.15)] w-full max-w-[370px] h-full max-h-56 aspect-video transition-all duration-700">
                 <div
                     class="absolute flex flex-col justify-start items-center bg-white py-5 w-full h-full transition-all group-hover:-translate-y-16 duration-300 group-hover:duration-1000">
                     <p class="font-serif font-semibold text-gray-500 text-xl sm:text-2xl">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./*.html", "./*.php", "./includes/*.php"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "instagram-gradient":
+          "linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%)",
+      },
+    },
   },
   plugins: [],
 };
